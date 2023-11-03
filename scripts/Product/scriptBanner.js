d270cba4652549574f791ebb42299e4375e2a681
Vũ Trường BÉ <vutruongbe25122003@gmail.com>
2023-11-03 23:46:42 +0700

    Update the Banner section on indexProduct.html

--- scripts/Product/scriptBanner.js
+++ scripts/Product/scriptBanner.js
@@ -3,6 +3,8 @@ document.addEventListener('DOMContentLoaded',function(){
     var load1 = document.querySelector('.cd-LR');
     var load2 = document.querySelector('.cd-BtT');
     var load3 = document.querySelector('.cd-RL');
+    var load4 = document.querySelector('.Ok-cd');
+    var load5 = document.querySelector('.Ok1-cd');
     var trangthais3 = "duoi";
 
     if(trangthais3=='duoi'){
@@ -11,11 +13,7 @@ document.addEventListener('DOMContentLoaded',function(){
         load1.classList.add('sangRight');
         load2.classList.add('lenTop');
         load3.classList.add('sangLeft');
+        load4.classList.add('sangLeft9');
+        load5.classList.add('sangLeft10');
     }
 },false)
-
-// $(function(){
-//     $('lentren').click(function(){
-//         $('body').animate({scrollTo:0})
-//     })
-// })
