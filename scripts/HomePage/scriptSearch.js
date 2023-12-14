5a126591f5a6c69a23c1a435464187d01985f9b8
Vũ Trường BÉ <vutruongbe25122003@gmail.com>
2023-12-14 08:17:59 +0700

    fix lại menu của các giao diện lần 3

--- scripts/HomePage/scriptSearch.js
+++ scripts/HomePage/scriptSearch.js
@@ -6,6 +6,7 @@ function MoTimKiem(x){
 function DongTimKiem(x){
     document.getElementById(x).style.opacity = 0;
     document.getElementById(x).style.width="0px";
+    dataList.innerHTML = '';
 }
 
 
@@ -19,16 +20,16 @@ const dataList = document.getElementById('DataList');
 let SanPham = [
     {   
         ten: "Túi xách Origen", gia: 800000, hinhAnh: "images/DM1.jpg", mauSac: "đen", 
-        thuongHieu: "Carit & Koty", vatLieu: "Vải canvas/cotton", kieuDang: "cầm tay",
+        thuongHieu: "Carit & Kotyim", vatLieu: "Vải canvas/cotton", kieuDang: "cầm tay",
         phongCach: "hiện đại", trangThai: "hot"
     },
     {   
-        ten: "Túi xách Positen V1", gia: 650000, hinhAnh: "images/DM2.jpg", mauSac: "đỏ", 
+        ten: "Túi xách Positen", gia: 650000, hinhAnh: "images/DM2.jpg", mauSac: "đỏ", 
         thuongHieu: "CAEISL & KCIEL", vatLieu: "Da", kieuDang: "balo",
         phongCach: "cổ điển", trangThai: "hot"
     },
     {   
-        ten: "Túi xách Pipomiria", gia: 690000, hinhAnh: "images/DM3.jpg", mauSac: "vàng", 
+        ten: "Túi xách Pipomi", gia: 690000, hinhAnh: "images/DM3.jpg", mauSac: "vàng", 
         thuongHieu: "URNVÔ REBAN", vatLieu: "Nhựa", kieuDang: "xách tay",
         phongCach: "cổ điển", trangThai: "bán chạy"
     },
@@ -38,8 +39,33 @@ let SanPham = [
         phongCach: "thời trang", trangThai: ""
     },
     {   
-        ten: "Túi xách Bustity Ome", gia: 720000, hinhAnh: "images/DM5.jpg", mauSac: "tím", 
-        thuongHieu: "Carit & Koty", vatLieu: "Vải canvas/cotton", kieuDang: "xách tay",
+        ten: "Túi xách Bustity", gia: 720000, hinhAnh: "images/DM5.jpg", mauSac: "tím", 
+        thuongHieu: "Carit & Kotyim", vatLieu: "Vải canvas/cotton", kieuDang: "xách tay",
+        phongCach: "hiện đại", trangThai: "giảm giá"
+    },
+    {   
+        ten: "Túi xách Bustity", gia: 720000, hinhAnh: "images/DM5.jpg", mauSac: "tím", 
+        thuongHieu: "Carit & Kotyim", vatLieu: "Vải canvas/cotton", kieuDang: "xách tay",
+        phongCach: "hiện đại", trangThai: "giảm giá"
+    },
+    {   
+        ten: "Túi xách Omizen", gia: 450000, hinhAnh: "images/DM4.jpg", mauSac: "xanh", 
+        thuongHieu: "CAEISL & KCIEL", vatLieu: "Vải canvas/cotton", kieuDang: "cầm tay",
+        phongCach: "thời trang", trangThai: ""
+    },
+    {   
+        ten: "Túi xách Bustity", gia: 720000, hinhAnh: "images/DM5.jpg", mauSac: "tím", 
+        thuongHieu: "Carit & Kotyim", vatLieu: "Vải canvas/cotton", kieuDang: "xách tay",
+        phongCach: "hiện đại", trangThai: "giảm giá"
+    },
+    {   
+        ten: "Túi xách Bustity", gia: 720000, hinhAnh: "images/DM5.jpg", mauSac: "tím", 
+        thuongHieu: "Carit & Kotyim", vatLieu: "Vải canvas/cotton", kieuDang: "xách tay",
+        phongCach: "hiện đại", trangThai: "giảm giá"
+    },
+    {   
+        ten: "Túi xách Bustity", gia: 720000, hinhAnh: "images/DM5.jpg", mauSac: "tím", 
+        thuongHieu: "Carit & Kotyim", vatLieu: "Vải canvas/cotton", kieuDang: "xách tay",
         phongCach: "hiện đại", trangThai: "giảm giá"
     },
 ];
@@ -70,12 +96,12 @@ dataTimKiem.addEventListener('input', function() {
         const li = document.createElement('li');
         li.title="Xem sản phẩm: "+`${item.ten}`;
         li.style.marginLeft="20px"
-        li.style.width="95%"
+        li.style.width="16%"
         li.style.float="left"
 
         const anhSP = document.createElement('img');
         anhSP.style.float="left"
-        anhSP.style.width="60px"
+        anhSP.style.width="160px"
         anhSP.style.marginLeft="10px"
         anhSP.style.marginRight="20px"
         anhSP.src = item.hinhAnh;
@@ -83,46 +109,54 @@ dataTimKiem.addEventListener('input', function() {
         const tenSP = document.createElement('span');
         tenSP.style.float="left"
         tenSP.style.marginTop="20px"
-        tenSP.textContent = `${item.ten} -`;
+        tenSP.style.marginLeft="30px"
+        tenSP.textContent = `${item.ten}`;
 
         
         const giaSP = document.createElement('span');
         giaSP.style.float="left"
         giaSP.style.marginLeft="5px"
         giaSP.style.marginTop="20px"
-        giaSP.textContent = `Giá: ${item.gia}đ ,`;
+        giaSP.style.marginLeft="35px"
+        giaSP.textContent = `Giá: ${item.gia}đ`;
 
         const mauSP = document.createElement('span');
         mauSP.style.float="left"
-        mauSP.style.marginLeft="5px"
+        mauSP.style.marginLeft="50px"
         mauSP.style.marginTop="20px"
-        mauSP.textContent = `Màu: ${item.mauSac} ,`;
+        mauSP.textContent = `Màu: ${item.mauSac}`;
 
         const thSP = document.createElement('span');
         thSP.style.float="left"
-        thSP.style.marginLeft="5px"
+        thSP.style.marginLeft="10px"
         thSP.style.marginTop="20px"
-        thSP.textContent = `Thương hiệu: ${item.thuongHieu} ,`;
+        thSP.textContent = `Nhà: ${item.thuongHieu}`;
 
-        const kieuSP = document.createElement('span');
-        kieuSP.style.float="left"
-        kieuSP.style.marginLeft="5px"
-        kieuSP.style.marginTop="20px"
-        kieuSP.textContent = `Kiểu: ${item.kieuDang} ,`;
+        // const kieuSP = document.createElement('span');
+        // kieuSP.style.float="left"
+        // kieuSP.style.marginLeft="5px"
+        // kieuSP.style.marginTop="20px"
+        // kieuSP.textContent = `Kiểu: ${item.kieuDang} ,`;
 
-        const pkSP = document.createElement('span');
-        pkSP.style.float="left"
-        pkSP.style.marginLeft="5px"
-        pkSP.style.marginTop="20px"
-        pkSP.textContent = `Phong cách: ${item.phongCach}`;
+        // const pkSP = document.createElement('button');
+        // pkSP.style.float="left"
+        // pkSP.style.marginLeft="40px"
+        // pkSP.style.marginTop="20px"
+        // pkSP.textContent = `Xem Sản Phẩm`;
+
+        // const mmSP = document.createElement('button');
+        // mmSP.style.float="left"
+        // mmSP.style.marginLeft="10px"
+        // mmSP.style.marginTop="20px"
 
         li.appendChild(anhSP);
         li.appendChild(tenSP);
         li.appendChild(giaSP);
         li.appendChild(mauSP);
         li.appendChild(thSP);
-        li.appendChild(kieuSP);
-        li.appendChild(pkSP);
+        // li.appendChild(mmSP);
+        // li.appendChild(kieuSP);
+        // li.appendChild(pkSP);
     
         li.addEventListener('click', function() {
             navigateToProductDetail(item);
