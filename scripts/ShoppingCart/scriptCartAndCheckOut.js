4870fad12cce4d409633553002365f771c0026c6
Vũ Trường BÉ <vutruongbe25122003@gmail.com>
2023-12-15 11:23:35 +0700

    Update tính năng về xử đơn hàng cho admin

--- scripts/ShoppingCart/scriptCartAndCheckOut.js
+++ scripts/ShoppingCart/scriptCartAndCheckOut.js
@@ -7,11 +7,12 @@ function GuiDuLieuThanhToan(){
     var soLuongCells = document.querySelectorAll('#chitietgiohang td:nth-child(5)');
     var thanhTienCells = document.querySelectorAll('#chitietgiohang td:nth-child(6)');
     var hinhCells = document.querySelectorAll('#chitietgiohang td:nth-child(2)');
+    var dongiaCells = document.querySelectorAll('#chitietgiohang td:nth-child(4)');
     for (let i = 0; i < tenCells.length; i++) {
         
         var itemSP = [i+1, tenCells[i].innerHTML, soLuongCells[i].innerHTML , thanhTienCells[i].innerHTML, hinhCells[i].innerHTML];
         arrSP.push(itemSP);
-        var itemHD = [i+1, hinhCells[i].innerHTML, tenCells[i].innerHTML, soLuongCells[i].innerHTML , thanhTienCells[i].innerHTML];
+        var itemHD = [i+1, hinhCells[i].innerHTML, tenCells[i].innerHTML, soLuongCells[i].innerHTML , thanhTienCells[i].innerHTML, dongiaCells[i].innerHTML];
         arrHD.push(itemHD);
     }
 
@@ -118,4 +119,105 @@ function showChoXacNhan() {
         choxn += "<tr>";
     }
     document.getElementById('chitietchoxacnhan').innerHTML=choxn;
+}
+
+function showDonHangAdmin() {
+    var gp = sessionStorage.getItem("arrHD");
+    var hoadon = JSON.parse(gp);
+    var gk = sessionStorage.getItem("nhanTienHD");
+
+    let TongDoooo = 0;
+
+    for (let i = 0; i < hoadon.length; i++) {
+        TongDoooo = parseInt(TongDoooo) + parseInt(hoadon[i][3]);
+    }
+
+    var chuoiChuyenDoi = gk;
+    var chuoiMoi = chuyenDoi(chuoiChuyenDoi);
+    // alert(TongDoooo + "-" +gk);
+
+    // Đơn hàng
+    var table = document.querySelector('.TableDH');
+    var newRow = document.createElement('tr');
+    var cell1 = document.createElement('td');
+        cell1.textContent = '1'; 
+        cell1.style.fontWeight = 'Bold'
+    var cell2 = document.createElement('td');
+        cell2.textContent = 'ĐH1';
+        cell2.style.fontWeight = 'Bold'
+    var cell3 = document.createElement('td');
+        cell3.textContent = 'KH1';
+        cell3.style.fontWeight = 'Bold'
+    var cell4 = document.createElement('td');
+        cell4.textContent = TongDoooo;
+        cell4.style.fontWeight = 'Bold'
+    var cell5 = document.createElement('td');
+        cell5.textContent = chuoiMoi;
+        cell5.style.fontWeight = 'Bold'
+    var cell6 = document.createElement('td');
+        cell6.textContent = 'Chờ xác nhận';
+        cell6.style.fontWeight = 'Bold'
+        cell6.style.color = 'blue'
+                        
+    var editCell = document.createElement('td');
+    var editButton = document.createElement('button');
+    editButton.type = 'button';
+    editButton.innerHTML = '<i class="fa-solid fa-pen" style="color: #005eff;"></i>';
+    editCell.appendChild(editButton);
+    var deleteCell = document.createElement('td');
+    var deleteButton = document.createElement('button');
+    deleteButton.type = 'button';
+    deleteButton.innerHTML = '<i class="fa-solid fa-trash" style="color: rgb(245, 45, 0);"></i>';
+    deleteButton.addEventListener('click', function() {
+        XoaLSP(this);
+    });
+    deleteCell.appendChild(deleteButton);
+
+    newRow.appendChild(cell1);
+    newRow.appendChild(cell2);
+    newRow.appendChild(cell3);
+    newRow.appendChild(cell4);
+    newRow.appendChild(cell5);
+    newRow.appendChild(cell6);
+    newRow.appendChild(editCell);
+    newRow.appendChild(deleteCell);
+    table.appendChild(newRow);
+
+
+    // CT Đơn hàng
+    for (let i = 0; i < hoadon.length; i++) {
+        
+        var tableb = document.querySelector('.TableCTDH');
+        var newRowb = document.createElement('tr');
+        var cell1b = document.createElement('td');
+            cell1b.textContent = i+1; 
+            cell1b.style.fontWeight = 'Bold'
+        var cell2b = document.createElement('td');
+            cell2b.textContent = 'ĐH1';
+            cell2b.style.fontWeight = 'Bold'
+        var cell3b = document.createElement('td');
+            cell3b.textContent = hoadon[i][2];
+            cell3b.style.fontWeight = 'Bold'
+        var cell4b = document.createElement('td');
+            cell4b.innerHTML = hoadon[i][1];
+            cell4b.style.fontWeight = 'Bold'
+        var cell5b = document.createElement('td');
+            cell5b.textContent = hoadon[i][3];
+            cell5b.style.fontWeight = 'Bold'
+        var cell6b = document.createElement('td');
+            cell6b.textContent = chuyenDoi(hoadon[i][5]);
+            cell6b.style.fontWeight = 'Bold'
+        var cell7b = document.createElement('td');
+            cell7b.textContent = chuyenDoi(hoadon[i][4]);
+            cell7b.style.fontWeight = 'Bold'
+    
+        newRowb.appendChild(cell1b);
+        newRowb.appendChild(cell2b);
+        newRowb.appendChild(cell3b);
+        newRowb.appendChild(cell4b);
+        newRowb.appendChild(cell5b);
+        newRowb.appendChild(cell6b);
+        newRowb.appendChild(cell7b);
+        tableb.appendChild(newRowb);
+    }
 }
